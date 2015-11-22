07627fdc66fa6cbab705991ca39f4fac9d09e4b5
Anton Verinov <git@zemlan.in>
2015-11-22 14:31:19 +0200

    loop incoming rtm messages

--- src/barberry/slack.clj
+++ src/barberry/slack.clj
@@ -4,16 +4,28 @@
             [byte-streams :as bs]
             [manifold.stream :as s]
             [clojure.data.json :as json]
+            [clojure.core.async :refer [go go-loop]]
             [barberry.config :refer [config]]))
 
 (def RTM-START-URL "https://slack.com/api/rtm.start")
+
+(defonce rtm-connection (atom nil))
 
 (defn bot-handshake [{ok :ok url :url :as resp}]
   (if-not ok
     (:error resp)
     (let [conn @(http/websocket-client url)]
       (println "rtm connected to" url)
-      (-> @(s/take! conn)))))
+      (when @rtm-connection
+        (s/close! @rtm-connection))
+      (reset! rtm-connection conn)
+      (go-loop []
+        (when-let [msg @(s/take! conn)]
+          (-> msg
+              (json/read-str :key-fn keyword)
+              println)
+          (recur)))
+      "bot is connected!")))
 
 (defn run-bot [& a]
   (-> @(http/get RTM-START-URL
